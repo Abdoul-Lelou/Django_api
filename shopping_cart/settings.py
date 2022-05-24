6579788f847ee5e31e0e673f8a76191840d93740
abdoul-lelou <abdourahmane.benzy@gmail.com>
2022-05-24 04:02:21 +0200

    settings

--- shopping_cart/settings.py
+++ shopping_cart/settings.py
@@ -104,15 +104,14 @@ DATABASES = {
      #}
 
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'django_api',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',                     
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'd6n05q0u5ibuon',
+        'USER': 'yfosdjtyeuudfx',
+        'PASSWORD': 'd0eba990517b7185095db1e2d0cbde560c110b91494bf13685b527137ae0a5a9',
+        'HOST': 'ec2-34-231-177-125.compute-1.amazonaws.com',                     
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
